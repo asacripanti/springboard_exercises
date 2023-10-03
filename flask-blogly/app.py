150b5be402b20099c3f30be5047f4ab41cdd2bfe
asacripanti <78769433+asacripanti@users.noreply.github.com>
2023-10-02 21:16:40 -0500

    Add files via upload

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, request, redirect, render_template, url_for
-from models import db, connect_db, User
+from models import db, connect_db, User, Post, Tag, PostTag
 from flask_debugtoolbar import DebugToolbarExtension
 
 # what does all this do?
@@ -26,7 +26,19 @@ if __name__ == "__main__":
 @app.route('/')
 def redirect_to_users():
     """Redirects to list of users."""
-    return redirect(url_for('show_all_users'))
+    return redirect(url_for('home_page'))
+
+
+@app.route('/home')
+def home_page():
+    """Home page"""
+    users = User.query.all()
+    posts = Post.query.all()
+    tags = Tag.query.all()
+    
+
+    return render_template('home.html', users=users, posts=posts, tags=tags)
+
     
 
 
@@ -46,6 +58,16 @@ def show_add_user_form():
 @app.route('/users/new', methods=['POST'])
 def add_user():
     """Add new user"""
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    # Create a new User object and add it to the database session
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+
     return render_template('new_user.html')
 
 
@@ -88,6 +110,100 @@ def users_destroy(user_id):
     db.session.commit()
 
     return redirect("/users")
+
+@app.route('/users/<int:user_id>/posts/<int:post_id>', methods=['GET'])
+def post_view(user_id, post_id):
+    """View post"""
+    user = User.query.get_or_404(user_id)
+    post = Post.query.get_or_404(post_id)
+    return render_template('post_view.html', user=user, post=post)
+
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=['GET'])
+def post_form(user_id):
+    """View form to add post"""
+
+    user = User.query.get_or_404(user_id)
+
+    post = Post()
+    return render_template('post_form.html', user=user, post=post)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def add_post(user_id):
+    """Submit form to add post"""
+
+    post = Post()
+
+    post.title = request.form['title']
+    post.content = request.form['content']
+    post.user_id = user_id 
+
+    tags = request.form['tags'].split(',')
+    for tag_name in tags:
+        tag_name = tag_name.strip()  # Remove leading/trailing whitespace
+        if tag_name:
+          # Check if the tag already exists
+            existing_tag = Tag.query.filter_by(name=tag_name).first()
+            if not existing_tag:
+                # If the tag does not exist, create a new Tag object and add it to the tags table
+                new_tag = Tag(name=tag_name)
+                db.session.add(new_tag)
+                existing_tag = new_tag  # Assign existing_tag to new_tag
+
+            # Create a new PostTag relationship and add it to the session
+            new_post_tag = PostTag(post=post, tag=existing_tag)
+            db.session.add(new_post_tag)
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.route('/users/<int:user_id>/posts/<int:post_id>/edit', methods=['GET'])
+def post_edit(user_id, post_id):
+    """Form to edit post"""
+
+    user = User.query.get_or_404(user_id)
+    post = Post.query.get_or_404(post_id)
+
+    return render_template('post_edit.html', user=user)
+
+
+@app.route('/users/<int:user_id>/posts/<int:post_id>/edit', methods=['POST'])
+def post_update(user_id, post_id):
+    """Form submission of edit form"""
+
+    user = User.query.get_or_404(user_id)
+    post = Post.query.get_or_404(post_id)
+
+    post.title = request.form['title']
+    post.content = request.form['content']
+    post.user_id = user_id 
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.route('/users/<int:user_id>/posts/<int:post_id>/delete', methods=['POST'])
+def post_delete(user_id, post_id):
+    """Delete post"""
+
+    
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect("/users") 
+
+
+
+
+
+
 
 
     
--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -23,6 +23,39 @@ class User(db.Model):
         return f"{self.first_name} {self.last_name}"
 
 
+class Post(db.Model):
+    __tablename__ = 'posts'     
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    # created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    user = db.relationship('User', backref='posts')
+
+
+class Tag(db.Model):
+    """tags"""
+
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, unique=True, nullable=False)
+
+class PostTag(db.Model):
+    """Post tags"""
+
+    __tablename__ = 'post_tags'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+    post = db.relationship('Post', backref='post_tags')
+    tag = db.relationship('Tag', backref='post_tags')
+
+
+
 def connect_db(app):
     """Connect database to provided Flask app"""
 
